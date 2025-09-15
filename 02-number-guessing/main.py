0769a4f26f73532ac8f43079ada64881db0667c3
Guilherme Rodrigues <mxtroy3@gmail.com>
2025-09-15 19:25:39 -0300

    update docstrings

--- 02-number-guessing/main.py
+++ 02-number-guessing/main.py
@@ -1,7 +1,21 @@
+"""
+Jogo de adivinha.
+O usuário escolhe inicialmente um alcance para gerar o número.
+Após o número ser gerado o jogo se inicia, onde o usuário deverá adivinhar o número gerado.
+O jogo se encerra quando o usuário adivinha o número gerado.
+"""
+
+# Realizando importação do módulo randint da biblioteca random
 from random import randint
 
 
 def choosing_range() -> tuple[int, int]:
+    """
+    Solicita ao usuário dois números.
+
+    Returns:
+        tuple(menor, maior): O menor e maior número a ser gerado.
+    """
     print("Vamos escolher o alcance do número que será gerado.\n")
     print("Você deverá selecionar o menor e o maior número possível.")
     print("---------------------------------------------------------------")
@@ -19,11 +33,20 @@ def choosing_range() -> tuple[int, int]:
 
 
 def generating_number(menor: int, maior: int) -> int:
+    """
+    Gera um número pseudo-aleatório com base em alcance determinado.
+
+    Returns:
+        Número inteiro gerado.
+    """
     numero_gerado = randint(menor, maior)
     return numero_gerado
 
 
 def guessing() -> int:
+    """
+    Executa o loop onde será recebido um número inteiro do usuário.
+    """
     while True:
         try:
             return int(input("\nDigite o seu palpite inicial: "))
@@ -32,6 +55,16 @@ def guessing() -> int:
 
 
 def playing(guess: int, numero_gerado: int) -> None:
+    """
+    Executa a lógica do jogo de adivinhação.
+
+    O usuário vai recebendo dicas se deve chutar um número maior ou menor.
+    Até acertar o número gerado.
+
+    Args:
+        guess: Primeiro palpite do usuário.
+        numero_gerado: Número que o usuário adeve adivinhar.
+    """
     contador = 1
     while True:
         if guess > numero_gerado:
