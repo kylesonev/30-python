a87e9ffec73522266754f6548cb683711e480e4e
Guilherme Rodrigues <mxtroy3@gmail.com>
2025-10-01 08:00:58 -0300

    weather app

---
+++ 26-weatherapp/main.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+API_KEY: Final[str] = "ee809d32587fbca61818dd8a86ae669e"
+BAS_URL: Final[str] = "api.openweathermap.org/data/2.5/forecast"
+
+ 
---
+++ 26-weatherapp/weather_api.py
@@ -0,0 +1,7 @@
+from typing import Final
+import requests
+
+
+API_KEY: Final[str] = "ee809d32587fbca61818dd8a86ae669e"
+BAS_URL: Final[str] = "api.openweathermap.org/data/2.5/forecast"
+
