51eb5af94b88c2c6b78ff282241698b06740e2ed
Guilherme Rodrigues <mxtroy3@gmail.com>
2025-10-06 18:23:30 -0300

    update

--- 03-dice-roll/main.py
+++ 03-dice-roll/main.py
@@ -1,13 +1,19 @@
 """
 Rolar dados
-O usuário insere a quantidade de dados de 6 lados que será rolado.
+O usuário insere a quantidade de dados de 6 lados que serão rolados.
 """
 
 
 from random import randint
+from collections import Counter
 
+def pegar_quantidade() -> int:
+    """
+    Solicita ao usuário a quantidade de dados de 6 lados que serão rolados.
 
-def get_quantity() -> int:
+    Returns:
+        int: a quantidade de dados que serão rolados.
+    """
     while True: 
         try:
             quantidade = int(input("\nQuantos dados gostaria de rolar? "))
@@ -21,23 +27,70 @@ def get_quantity() -> int:
         except ValueError:
             print("Digite um número válido! ")
 
+def rolar_dados(quantidade: int) -> list[int]:
+    """
+    Rola a quantidade de dados especificada, gerando números aleatórios entre 1 e 6.
 
-def rolling_dice(quantidade: int) -> list[int]:
-    rolls = []
+    Args:
+        quantidade(int): a quantidade de dados que serão rolados.
+    Returns: 
+        list[int]: lista contendo os resultados dos dados rolados.
+
+    """
+    dados = []
     for _ in range(quantidade):
-        rolls.append(randint(1, 6))
-    return rolls
+        dados.append(randint(1, 6))
+    return dados
+
+
+def mostrar_resultados(dados: list[int]) -> None:
+    """
+    Mostra os resultados obtidos ao rolar os dados.
+    Args:
+        dados(list[int]): lista contendo os resultados dos dados rolados.
+    """
+    quantidade = len(dados)
+    print(f"\nResultado {'do dado' if quantidade == 1 else 'dos dados'}:")
+
+    for i, dado in enumerate(dados, start=1):
+            print(f"{i}° dado: {dado} ")
+    
+
+
+def mostrar_frequencia(opcao: str, dados: list[int]) -> None:
+    """
+    Mostra a frequência de cada dado contido na lista de dados rolados.
+    Args:
+        opcao(str): a opção inserida pelo usuário.
+        dados(list[int]): lista contendo os resultados dos dados rolados.
+    """
+    try:
+        if opcao in ('s', 'sim'):
+            contagem = Counter(dados)
+            print("\nFrequência dos números: ")
+            for numero in range(1, 7):
+                print(f"Número {numero}: {contagem.get(numero, 0)} {'vez' if contagem.get(numero, 0) <= 1 else 'vezes'}")
+        elif opcao in ('n', 'não'):
+            pass
+    except ValueError:
+        print("Entrada inválida!")
 
 
 def main():
+    """
+    Controla o fluxo principal do programa:
+    1. Solicita a quantidade de dados a serem rolados.
+    2. Exibe os resultados.
+    3. Pergunta se o usuário quer ver a frequência dos números.
+    4. Pergunta se deseja jogar novamente.
+    """
     while True:
-        quantidade = get_quantity()
-        dados = rolling_dice(quantidade)
-        contador = 1
-        for dado in dados:
-            print(f"{contador}° dado: {dado} ")
-            contador += 1
-        
+        quantidade = pegar_quantidade()
+        dados = rolar_dados(quantidade)
+        mostrar_resultados(dados)
+        opcao = input("\nGostaria de ver a frequência dos números? (s/n)").lower().strip()
+        mostrar_frequencia(opcao, dados)
+                   
         while True:
             try:
                 play_again = input("\nGostaria de rolar novamente? (s/n)").lower().strip()                
