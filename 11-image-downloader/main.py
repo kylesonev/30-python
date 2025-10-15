48df145611fc101ee6f20ad924a18bf813780d82
Guilherme Rodrigues <mxtroy3@gmail.com>
2025-10-15 20:21:00 -0300

    updatessss

--- 11-image-downloader/main.py
+++ 11-image-downloader/main.py
@@ -1,21 +1,39 @@
+"""
+Baixador de imagens
+"""
+
 import os
 
 import requests
 
 
-def get_extension(image_url: str) -> str | None:
+def pegar_extensao(image_url: str) -> str | None:
+    """
+    Pega a extensão da imagem no link passado.
+    Args:
+        image_url(str): url da imagem que será baixada
+
+    """
     extensions: list[str] = [".png", ".jpeg", ".jpg", ".gif", ".svg"]
     for ext in extensions:
         if ext in image_url:
             return ext
 
 
-def download_image(image_url: str, name: str, folder: str = None):
-    if ext := get_extension(image_url):
+def download_imagem(imagem_url: str, nome: str, folder=None):
+    """
+    Realiza o download da imagem
+    Args:
+        image_url(str): url da imagem que será baixada
+        nome: nome do arquivo que será salva a imagem
+        folder: nome da pasta para baixar a imagem
+    """
+
+    if ext := pegar_extensao(imagem_url):
         if folder:
-            image_name: str = f"{folder}/{name}{ext}"
+            image_name: str = f"{folder}/{nome}{ext}"
         else:
-            image_name: str = f"{name}{ext}"
+            image_name: str = f"{nome}{ext}"
 
     else:
         raise Exception("A extenção da imagem não foi localizada!")
@@ -26,7 +44,7 @@ def download_image(image_url: str, name: str, folder: str = None):
 
     # Baixa a imagem
     try:
-        image_content: bytes = requests.get(image_url).content
+        image_content: bytes = requests.get(imagem_url).content
         with open(image_name, "wb") as handler:
             handler.write(image_content)
             print(f"Baixado com sucesso: {image_name}")
@@ -36,9 +54,14 @@ def download_image(image_url: str, name: str, folder: str = None):
 
 
 if __name__ == "__main__":
+    """
+    Executa a lógica do programa
+    1. Solicita ao usuário a url da imagem a ser baixada.
+    2. Solicita ao usuário o nome do arquivo para salvar a imagem
+    3. Realiza o donwload da imagem
+    """
     input_url: str = input("Insira a url da imagem: ")
     input_name: str = input("Digite um nome para o arquivo a ser salvo: ")
 
     print("Baixando...")
-    download_image(image_url=input_url, name=input_name, folder="images")
-
+    download_imagem(imagem_url=input_url, nome=input_name, folder="images")
