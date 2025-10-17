540ef97cced32ecdab9efda506a34fb1c8f58f31
Guilherme Rodrigues <mxtroy3@gmail.com>
2025-10-17 19:07:09 -0300

    updatessss

--- 13-file-sorter/main.py
+++ 13-file-sorter/main.py
@@ -1,9 +1,19 @@
+"""
+Organiza arquivos dado um path
+Utiliza a extensão dos arquivos existentes no path para criar diretórios baseados em suas extensões
+"""
+
 import os
 import shutil
 
 
-def create_folder(path: str, extension: str) -> str:
-    """Cria uma pasta que é nomeada baseada na extenção do arquivo que é passado"""
+def criar_diretorio(path: str, extension: str) -> str:
+    """
+    Cria uma pasta que é nomeada baseada na extenção do arquivo que é passado
+    Args:
+        path(str): caminho onde será criado o diretório
+        extension(str): extensão do arquivo para nomear diretório
+    """
     folder_name: str = extension[1:]
     folder_path: str = os.path.join(path, folder_name)
 
@@ -13,8 +23,12 @@ def create_folder(path: str, extension: str) -> str:
     return folder_path
 
 
-def sort_files(source_path: str):
-    """Organiza arquivos baseado no caminho dado"""
+def organizar_arquivos(source_path: str) -> None:
+    """
+    Organiza arquivos baseado no caminho dado
+    Args:
+        source_path(str): caminho de origem onde serão organizados os arquivos
+    """
 
     for root_dir, sub_dir, filenames in os.walk(source_path):
         for filename in filenames:
@@ -22,14 +36,19 @@ def sort_files(source_path: str):
             extension: str = os.path.splitext(filename)[1]
 
             if extension:
-                target_folder: str = create_folder(source_path, extension)
+                target_folder: str = criar_diretorio(source_path, extension)
                 target_path: str = os.path.join(target_folder, filename)
 
                 shutil.move(file_path, target_path)
 
 
-def remove_empty_folder(source_path: str):
-    """Remove todos as pastas vazias"""
+def remover_diretorio_vazio(source_path: str) -> None:
+    """
+    Remove todos as pastas vazias
+    Args:
+        source_path(str): caminho de origem onde serão removidas as pastas vazias
+
+    """
 
     for root_dir, sub_dir, filenames in os.walk(source_path, topdown=False):
         for current_dir in sub_dir:
@@ -40,11 +59,17 @@ def remove_empty_folder(source_path: str):
 
 
 def main():
+    """
+    Executa a lógica do programa
+    1. Solicita ao usuário o path onde serão organizados os arquivos
+    2. Verifica se o path existe
+    3. Organiza os arquivos
+    """
     user_input: str = input("Informe o path do arquivo para organizar: ")
 
     if os.path.exists(path=user_input):
-        sort_files(user_input)
-        remove_empty_folder(user_input)
+        organizar_arquivos(user_input)
+        remover_diretorio_vazio(user_input)
         print("Arquivos organizados")
     else:
         print("Caminho inválido!")
